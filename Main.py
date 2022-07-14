05d1ec53be673289635ec47c632fa2d9dd5e51b2
Kunal Chauhan <87843911+Doombazooka@users.noreply.github.com>
2022-07-14 15:43:41 +0530

    Update Main.py
    Added more feature and other language support

--- Main.py
+++ Main.py
@@ -1,7 +1,54 @@
 from googletrans import Translator
+
 translate = Translator()
 text = str(input("Please enter the text you want to convert : "))
-detectedLang=(translate.detect(text))
-print("\nThe Langauge That you entred is ", detectedLang.lang)
-out = translate.translate(text, dest='en')
+detectedLang = (translate.detect(text))
+lang: str = "en"
+if detectedLang.lang == "hi":
+    detectedLang = "Hindi"
+elif detectedLang.lang == "bn":
+    detectedLang = "Bengali"
+elif detectedLang.lang == "mr":
+    detectedLang = "Marathi"
+elif detectedLang.lang == "mr":
+    detectedLang = "Marathi"
+elif detectedLang.lang == "te":
+    detectedLang = "Telugu"
+elif detectedLang.lang == "ta":
+    detectedLang = "Tamil"
+elif detectedLang.lang == "ur":
+    detectedLang = "Urdu"
+elif detectedLang.lang == "gu":
+    detectedLang = "Gujarati"
+print("\nThe Langauge That you entered is ", detectedLang)
+print("Do you want to translate other language rather than english types yes")
+choice = str(input())
+if choice == "yes" or choice == "Yes" or choice == "YES":
+    print('\n Please Enter the langauge you want to translate')
+    print(
+        "For Hindi type hi\nFor english type en \nFor Urdu type ur \nFor Tamil type ta\nFor Telugu type te\nFor "
+        "Matathi "
+        "type mr\nFor gangling type bn")
+    lang = str(input())
+out = translate.translate(text, dest=lang)
+detectedLang = out.dest
+if detectedLang == "hi":
+    detectedLang = "Hindi"
+elif detectedLang == "bn":
+    detectedLang = "Bengali"
+elif detectedLang == "mr":
+    detectedLang = "Marathi"
+elif detectedLang == "mr":
+    detectedLang = "Marathi"
+elif detectedLang == "te":
+    detectedLang = "Telugu"
+elif detectedLang == "ta":
+    detectedLang = "Tamil"
+elif detectedLang == "ur":
+    detectedLang = "Urdu"
+elif detectedLang == "gu":
+    detectedLang = "Gujarati"
+elif detectedLang == "en":
+    detectedLang = "English"
+print("Your Text in ", detectedLang, "is")
 print("\nThe Translation is ", out.text)
